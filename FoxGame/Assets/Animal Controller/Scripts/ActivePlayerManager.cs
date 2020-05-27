99db91c7f6d2fa24b152fbacbbfa5cc2f43e47cb
Philipp-sama123 <Philipp.Stampfer@protonmail.ch>
2020-05-28 01:18:29 +0200

    adjust buttons

---
+++ FoxGame/Assets/Animal Controller/Scripts/ActivePlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivePlayerManager : MonoBehaviour
+{
+    [SerializeField] GameObject racoonController;
+    [SerializeField] GameObject foxController;
+
+    public GameObject RacoonController { get => racoonController; set => racoonController = value; }
+    public GameObject FoxController { get => foxController; set => foxController = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(racoonController);
+        Debug.Log(foxController); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
