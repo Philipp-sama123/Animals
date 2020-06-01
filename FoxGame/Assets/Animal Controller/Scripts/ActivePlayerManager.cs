aff4fd846f63391578879af83e9d131455e41efe
Philipp-sama123 <Philipp.Stampfer@protonmail.ch>
2020-06-01 23:23:25 +0200

    adjust ActivePlayerScript

--- FoxGame/Assets/Animal Controller/Scripts/ActivePlayerManager.cs
+++ FoxGame/Assets/Animal Controller/Scripts/ActivePlayerManager.cs
@@ -4,45 +4,35 @@ using UnityEngine;
 using Cinemachine;
 using MalbersAnimations.Controller;
 using MalbersAnimations.Events;
+using UnityEngine.AI;
 
 public class ActivePlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject racoonController;
     [SerializeField] GameObject foxController;
-
-
+    [SerializeField] MalbersAnimations.ModeID modeIdRaccoon;
+    [SerializeField] float stoppingDistanceFox = 1f; 
     public CinemachineVirtualCamera followCam;
     public GameObject RacoonController { get => racoonController; set => racoonController = value; }
     public GameObject FoxController { get => foxController; set => foxController = value; }
 
-    private bool isRacoonActivePlayer = true; 
+    private bool isRacoonActivePlayer = true;
     // Start is called before the first frame update
     void Start()
     {
-        followCam = this.gameObject.GetComponent<CinemachineVirtualCamera>();
-
-        followCam.LookAt = racoonController.transform;
-        followCam.Follow = racoonController.transform;
-        //  virtualCamera = (CinemachineVirtualCamera)FindObjectOfType(typeof(CinemachineVirtualCamera));
-        // virtualCamera = this.gameObject.GetCinemachineComponent<CinemachineVirtualCamera>();
-        //(foxController.GetComponent<MEventListener>()).enabled = false;
-        //(foxController.GetComponent<MAnimalAIControl>()).enabled = true;
-        SetActivePlayerRaccoon(); 
-        Debug.Log(racoonController);
-        Debug.Log(foxController);
+        isRacoonActivePlayer = true;
+        SetActivePlayerRaccoon();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        isRacoonActivePlayer = racoonController.GetComponent<MAnimal>().isPlayer.Value;
+        Debug.Log(isRacoonActivePlayer);
     }
 
     public void ChangeActivePlayer()
-    {   
+    {
         //todo: maybe change the tag of the animal played(!) 
-        followCam = this.gameObject.GetComponent<CinemachineVirtualCamera>();
-        Debug.Log(isRacoonActivePlayer);
         if (isRacoonActivePlayer)
         {
             SetActivePlayerFox();
@@ -55,38 +45,61 @@ public class ActivePlayerManager : MonoBehaviour
 
     private void SetActivePlayerFox()
     {
-        isRacoonActivePlayer = false; 
-        followCam.LookAt = foxController.transform;
-        followCam.Follow = foxController.transform;
+        isRacoonActivePlayer = false;
 
-        (foxController.GetComponent<MEventListener>()).enabled = true;
-        (foxController.GetComponent<MAnimalAIControl>()).enabled = false;
-        (foxController.GetComponent<UnityEventRaiser>()).enabled = true;
+        ActivateControlsForFox();
+        DisableControlsForRaccoon();
 
-        (racoonController.GetComponent<MEventListener>()).enabled = false;
-    //    (racoonController.GetComponent<MAnimalAIControl>()).enabled = true;
-    //    (racoonController.GetComponent<MAnimalAIControl>()).SetTarget(racoonController);
 
-        (racoonController.GetComponent<MEventListener>()).enabled = false;
-        (racoonController.GetComponent<UnityEventRaiser>()).enabled = false;
     }
 
     private void SetActivePlayerRaccoon()
     {
         isRacoonActivePlayer = true;
-        followCam.LookAt = racoonController.transform;
-        followCam.Follow = racoonController.transform;
 
+        ActivateControlsForRaccoon();
+        DisableControlsForFox();
+    }
+
+    private void DisableControlsForFox()
+    {
+        (foxController.GetComponent<MAnimal>()).isPlayer.Value = false;
+        (foxController.GetComponent<MEventListener>()).enabled = false;
+        // set AI 
+        MAnimalAIControl aiControlFox = (foxController.GetComponent<MAnimalAIControl>());
+        aiControlFox.enabled = true;
+        aiControlFox.SetTarget(racoonController.transform, true);
+        aiControlFox.StoppingDistance = stoppingDistanceFox;
+    }
+
+    private void DisableControlsForRaccoon() // fox! (make dynamic)
+    {
+        MAnimal mAnimalRaccoon = racoonController.GetComponent<MAnimal>();
+        mAnimalRaccoon.isPlayer.Value = false;
+        mAnimalRaccoon.Mode_Interrupt();
+        mAnimalRaccoon.Mode_Pin(modeIdRaccoon);
+        mAnimalRaccoon.Mode_Pin_Input(true);
+
+        (racoonController.GetComponent<MEventListener>()).enabled = false;
+    }
+    private void ActivateControlsForFox()
+    {
+        SetCameraToActiveAnimal(foxController.transform);
+        (foxController.GetComponent<MAnimal>()).isPlayer.Value = true;
+        (foxController.GetComponent<MEventListener>()).enabled = true;
+        (foxController.GetComponent<MAnimalAIControl>()).enabled = false;
+    }
+
+    private void ActivateControlsForRaccoon()
+    {
+        SetCameraToActiveAnimal(racoonController.transform);
+        (racoonController.GetComponent<MAnimal>()).isPlayer.Value = true;
         (racoonController.GetComponent<MEventListener>()).enabled = true;
-       // (racoonController.GetComponent<MAnimalAIControl>()).enabled = false;
-        (racoonController.GetComponent<UnityEventRaiser>()).enabled = true;
+    }
 
-        (foxController.GetComponent<MEventListener>()).enabled = false;
-        (foxController.GetComponent<MAnimalAIControl>()).enabled = true;
-        (foxController.GetComponent<MAnimalAIControl>()).SetTarget(foxController);
-
-        (foxController.GetComponent<MEventListener>()).enabled = false;
-        (foxController.GetComponent<UnityEventRaiser>()).enabled = false;
-
+    private void SetCameraToActiveAnimal(Transform activeAnimalTransform)
+    {
+        followCam.LookAt = activeAnimalTransform;
+        followCam.Follow = activeAnimalTransform;
     }
 }
