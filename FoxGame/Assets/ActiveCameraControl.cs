19e0cc89b4f9e46778d406bc023d6964e52f5095
Philipp-sama123 <Philipp.Stampfer@protonmail.ch>
2020-07-25 07:23:21 +0200

    customize first level

---
+++ FoxGame/Assets/ActiveCameraControl.cs
@@ -0,0 +1,20 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveCameraControl : MonoBehaviour
+{
+    [SerializeField] CinemachineFreeLook freeLookCamera;
+    // Update is called once per frame
+    private void Start()
+    {
+        if (freeLookCamera == null)
+            freeLookCamera = GameObject.FindObjectOfType<CinemachineFreeLook>(); 
+    }
+
+    public void toggleCameraActivity()
+    {
+        freeLookCamera.enabled = !freeLookCamera.enabled; 
+    }
+}
