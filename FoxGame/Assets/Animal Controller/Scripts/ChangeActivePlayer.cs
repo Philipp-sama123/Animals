10a490ad0b9e6d198d39aef012fc5481b4ca98d5
Philipp-sama123 <Philipp.Stampfer@protonmail.ch>
2020-09-06 22:06:18 +0200

    add changing of player

--- FoxGame/Assets/Animal Controller/Scripts/ChangeActivePlayer.cs
+++ FoxGame/Assets/Animal Controller/Scripts/ChangeActivePlayer.cs
@@ -1,6 +1,8 @@
 ﻿using Cinemachine;
+using MalbersAnimations;
 using MalbersAnimations.Controller;
 using MalbersAnimations.Events;
+using MalbersAnimations.Scriptables;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +10,20 @@ using UnityEngine;
 public class ChangeActivePlayer : MonoBehaviour
 {
     [SerializeField] private GameObject CurrentAnimal;
-    [SerializeField] private GameObject AnimalPrefab;
+    [SerializeField] private GameObject AnimalPrefab;// todo probably unnecessary
     [SerializeField] private GameObject NextAnimalPrefab;
 
-    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject[] AnimalPrefabList;
+
+    [SerializeField] private ModeID modeID;
+    [SerializeField] private Action ActionID;
+
     private CinemachineVirtualCamera followCamera;
     private CinemachineFreeLook freeLookCamera;
-    public AnimalEvent OnZoneActivation = new AnimalEvent();
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
@@ -26,7 +31,20 @@ public class ChangeActivePlayer : MonoBehaviour
     {
 
     }
-
+    public void SelectNextAnimalPrefab()
+    {
+        foreach (GameObject animal in AnimalPrefabList)
+        {
+            if (CurrentAnimal.GetComponent<AnimalController>().AnimalType != animal.GetComponent<AnimalController>().AnimalType)
+            {
+                AnimalPrefab = animal;
+            }
+            else
+            {
+                NextAnimalPrefab = animal;
+            }
+        }
+    }
 
     public void ChangeAnimal()
     {
@@ -39,10 +57,10 @@ public class ChangeActivePlayer : MonoBehaviour
         AnimalController[] players = FindObjectsOfType<AnimalController>();
         foreach (AnimalController player in players)
         {
-            Debug.LogError("Player" + player.ToString());
             if (!player.IsPlayerActive) player.DestroyGameObject(3);
         }
-        AnimalPrefab = NextAnimalPrefab;
+        SelectNextAnimalPrefab();
+        // AnimalPrefab = NextAnimalPrefab;
     }
 
     private void DisableOldAnimal()
@@ -50,17 +68,22 @@ public class ChangeActivePlayer : MonoBehaviour
         CurrentAnimal.GetComponent<MEventListener>().enabled = false;
         CurrentAnimal.GetComponent<MAnimal>().isPlayer.Value = false;
         CurrentAnimal.GetComponent<AnimalController>().IsPlayerActive = false;
-        // todo stop moving
-        // todo force sleeping mode
+        StopMoving();
+    }
+
+    private void StopMoving()
+    {
+        CurrentAnimal.GetComponent<MAnimal>().MovementAxis = CurrentAnimal.GetComponent<MAnimal>().MovementAxisSmoothed = Vector3.zero;
+        CurrentAnimal.GetComponent<MAnimal>().Mode_Activate(modeID, ActionID);
     }
 
     private void InstantiateNewAnimal()
     {
-        GameObject newAnimal = Instantiate(AnimalPrefab, spawnPoint.transform.position, Quaternion.identity);
- 
+        GameObject newAnimal = Instantiate(AnimalPrefab, new Vector3(CurrentAnimal.transform.position.x, CurrentAnimal.transform.position.y + 1, CurrentAnimal.transform.position.z), Quaternion.identity);
+
         newAnimal.GetComponent<MAnimal>().SetMainPlayer();
         newAnimal.GetComponent<MEventListener>().enabled = true;
-        CurrentAnimal = newAnimal; 
+        CurrentAnimal = newAnimal;
 
         freeLookCamera = GameObject.FindGameObjectWithTag("FreeLookCamera").GetComponent<CinemachineFreeLook>();
         followCamera = GameObject.FindGameObjectWithTag("FollowCamera").GetComponent<CinemachineVirtualCamera>();
